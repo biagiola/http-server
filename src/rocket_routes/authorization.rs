cc1d549091c9c8c57245b4e9c6de0266417b35b6
David Biagiola <marcelobiagiola01@gmail.com>
2025-10-23 17:30:17 -0300

    implement redis key/value set with expire

--- src/rocket_routes/authorization.rs
+++ src/rocket_routes/authorization.rs
@@ -2,17 +2,28 @@ use crate::rocket_routes::{CacheConn, DbConn, server_error};
 use crate::repositories::UserRepository;
 use crate::auth::{Credentials, authorize_user};
 use rocket::serde::json::{json, Json, Value};
+use rocket::http::Status;
+use rocket_db_pools::deadpool_redis::redis::AsyncCommands;
 use rocket_db_pools::Connection;
 use rocket::response::status::Custom;
 
 #[rocket::post("/login", format="json", data="<credentials>")]
 pub async fn login(mut db: Connection<DbConn>, mut cache: Connection<CacheConn>, credentials: Json<Credentials>) -> Result<Value, Custom<Value>> {
-    UserRepository::find_by_username(&mut db, &credentials.username).await
-        .map(|user| {
-            if let Ok(token) = authorize_user(&user, credentials.into_inner()) {
-                return json!(token);
-            }
-            json!("Unauthorized")
-        })
-        .map_err(|e| server_error(e.into()))
+    let user = UserRepository::find_by_username(&mut db, &credentials.username).await
+        .map_err(|e| server_error(e.into()))?;
+
+    let session_id = authorize_user(&user, credentials.into_inner())
+        .map_err(|_|Custom(Status::Unauthorized, json!("Unauthorized")))?;
+
+    // insert into redis. TODO: implement redis repository
+    cache.set_ex::<String, i32, usize>(
+        format!("sessions/{}", session_id),
+        user.id,
+        3*60*60
+    ).await
+    .map_err(|e| server_error(e.into()))?;
+
+    Ok(json!({
+        "token": session_id,
+    }))
 }
